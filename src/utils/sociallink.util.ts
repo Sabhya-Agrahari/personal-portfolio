31b9f4c5b4803c891f76720fd74ddb3f69a21283
Sabhya Agrahari <sabhyaagrahari18@gmail.com>
2026-05-07 10:44:23 +0530

    version3

---
+++ src/utils/sociallink.util.ts
@@ -0,0 +1,4 @@
+export const GitHubLink: string = "https://github.com/sabhya-agrahari";
+export const LinkedInLink: string = "https://www.linkedin.com/in/sabhya-agrahari-1864621a8";
+export const InstagramLink: string = "https://www.instagram.com/sabhya_agrahari";
+export const GmailLink: string = "mailto:sabhyaagrahari18@gmail.com";
